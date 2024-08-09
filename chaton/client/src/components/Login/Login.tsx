6f89c5aba43732b2c4c2c500dd5dda1a3a84e4a0
grosdamir <126281062+damirgros@users.noreply.github.com>
2024-08-09 19:10:06 +0200

    Update build

--- chaton/client/src/components/Login/Login.tsx
+++ chaton/client/src/components/Login/Login.tsx
@@ -14,10 +14,7 @@ const Login: React.FC = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await axios.post("https://chaton-f1o0.onrender.com/api/auth/login", {
-        email,
-        password,
-      });
+      const response = await axios.post(`${REACT_APP_API_URL}/api/auth/login`, { email, password });
       if (response.data.redirectUrl) {
         navigate(response.data.redirectUrl);
       }
--- chaton/server/src/server.js
+++ chaton/server/src/server.js
@@ -23,7 +23,12 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://chaton-client.onrender.com",
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
